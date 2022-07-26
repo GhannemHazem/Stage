b399852504bd33710fc0932427238d239f4d7103
GhannemHazem <hazemmega55@gmail.com>
2022-07-26 11:29:28 +0100

    Auth

---
+++ backend/Models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    isadmin: {type: Boolean , required: true, default:false},
+    email: {type: String , required: true , unique: true} ,
+    firstName: {type: String , required: true},
+    password: {type: String , required: true},
+    lastName: {type: String , required: true},
+    phone: {type: String ,require: true },
+    image: {type: String },
+    createdAt: {type: Date , default: Date.now}
+})
+module.exports = mongoose.model('User', UserSchema)
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,178 @@
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+import FileBase64 from 'react-file-base64';
+
+function Register() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName:'',
+    phone:'',
+    image:'',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const { firstName,lastName, phone, image, email, password, password2 } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        firstName,
+        lastName,
+        phone,
+        image,
+        email,
+        password,
+      }
+
+      dispatch(register(userData))
+    }
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='firstName'
+              name='firstName'
+              value={firstName}
+              placeholder='Enter your name'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='lastName'
+              name='lastName'
+              value={lastName}
+              placeholder='lastName'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='phone'
+              name='phone'
+              value={phone}
+              placeholder='phone'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='image'
+              name='image'
+              value={image}
+              placeholder='image'
+              onChange={onChange}
+            />
+          </div>
+          {/* <div className='form-group'>
+          <FileBase64
+                multiple={ false }
+                
+                onDone={ ({ base64 }) => setFormData({ ...formData, image: base64 }) } />
+            
+          </div> */}
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value={password2}
+              placeholder='Confirm password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Register
