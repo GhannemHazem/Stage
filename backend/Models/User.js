147155fb1f83e8d78db1ceee60bd2891be8ea82f
GhannemHazem <hazemmega55@gmail.com>
2022-08-04 10:54:21 +0200

    comment

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     isadmin: {type: Boolean , required: true, default:false},
     email: {type: String , required: true , unique: true} ,
     firstName: {type: String , required: true},
-    password: {type: String , required: true},
+    password: {type: String , required: true}, // make password unreachable by add select: false
     lastName: {type: String , required: true},
     phone: {type: String ,require: true },
     image: {type: String },
