9d37802932719c45506de96274c15e0c186490c9
GhannemHazem <hazemmega55@gmail.com>
2022-08-07 05:18:54 +0200

    CreateUpdateProduct

--- backend/Controllers/ProductController.js
+++ backend/Controllers/ProductController.js
@@ -32,4 +32,52 @@ const AdminDeleteProduct = asyncHandler(async (req, res) => {
       throw new Error('product not deleted')
     }
   })
-module.exports = {getProducts ,getProductId,AdminDeleteProduct}
+
+
+  const CreateProduct = asyncHandler(async (req, res) => {
+   
+    const product = new products({
+      name:'sample name',
+      user: req.user._id,
+      image: '/image/sample.jpd',
+      brand:'sample brand',
+      category:'sample category',
+      description:'sample description',
+    })
+    const createproduct = await product.save()
+    res.status(201).json(createproduct )
+    
+  })
+
+
+  const UpdateProduct = asyncHandler(async (req, res) => {
+    const { name, brand, category, image, description,price,stock} = req.body
+  
+    
+   const product = await products.findById(req.params.id)
+
+   if (product) {
+    product.name=name
+    product.brand=brand
+    product.category=category
+    product.image=image
+    product.description=description
+    product.price=price
+    product.stock=stock
+
+
+    const updateproduct = await product.save()
+    res.json(updateproduct)
+   }else{
+    res.status(404)
+    throw new Error ('Product not found')
+   }
+    
+  })
+  
+
+
+
+
+
+module.exports = {getProducts ,getProductId,AdminDeleteProduct,UpdateProduct,CreateProduct}
--- backend/Routes/ProductRoutes.js
+++ backend/Routes/ProductRoutes.js
@@ -3,13 +3,15 @@ const router = express.Router()
 const products = require('../Models/Product')
 const { protect,isAdmin } = require('../middleware/authMiddleware')
 
-const { getProducts, getProductId,AdminDeleteProduct } = require('../Controllers/ProductController')
+const { getProducts, getProductId,AdminDeleteProduct,CreateProduct,UpdateProduct } = require('../Controllers/ProductController')
 
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect,isAdmin,CreateProduct)
 router.route('/:id')
 .get(getProductId)
 .delete(protect,isAdmin,AdminDeleteProduct)
+.put(protect,isAdmin,UpdateProduct)
+
 
 
 
